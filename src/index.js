b977cd66e51dbe5f414d14dcac6654be0fafba19
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-12-13 06:29:08 -0700

    Building backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to // DEBUG:
+
+mongoose.connect(db)
+  .then((res) => {
+    console.log('database connect');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+app.get('/', (req, res) => {
+  res.send("hello dude ");
+});
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('listening on the port');
+})
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+//
+// ReactDOM.render(<App />, document.getElementById('app'));
